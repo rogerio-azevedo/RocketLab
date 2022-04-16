bf0a866af2987a00cf8acd902bb9bdc6633bddbe
rfazevedo <rogerio.francis@gmail.com>
2022-04-16 15:18:35 -0400

    First class Rocketseat Lab

---
+++ purchases/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { PrismaService } from './database/prisma/prisma.service';
+import { HttpModule } from './http/http.module';
+
+@Module({
+  imports: [DatabaseModule, HttpModule],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
